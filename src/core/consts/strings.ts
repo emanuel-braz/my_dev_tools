1f0b48072e326b60e4ea82ade255a6cff24fb291
Raí Miyashiro <52873303+raimiyashiro@users.noreply.github.com>
2022-01-03 00:05:23 -0300

    strings: replace infinitive by present continuous

--- src/core/consts/strings.ts
+++ src/core/consts/strings.ts
@@ -1,5 +1,5 @@
 export const APP_PLACEHOLDER_ROUTE = "Enter the route. e.g. scheme://my.route";
-export const APP_EXEC_NOT_FOUND = '{exec} not found! Please install it, before run this command again';
+export const APP_EXEC_NOT_FOUND = '{exec} not found! Please install it before running this command again';
 export const APP_STATUS_BAR_ITEM = `$(link) Deep Link`;
 export const APP_STATUS_BAR_ITEM_HINT = "it runs the latest deep link command";
 export const APP_LOCAL_CACHE_CLEARED = "The local cache was cleared";
